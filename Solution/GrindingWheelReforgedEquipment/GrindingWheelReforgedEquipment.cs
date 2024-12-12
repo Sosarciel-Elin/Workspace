d314bfa978f37d4e8008200fec7abd0c704aa90b
zwa73 <ssfen2419@126.com>
2024-12-12 22:12:09 +0800

    upg

--- Solution/GrindingWheelReforgedEquipment/GrindingWheelReforgedEquipment.cs
+++ Solution/GrindingWheelReforgedEquipment/GrindingWheelReforgedEquipment.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using System;
 using System.Collections.Generic;
@@ -8,23 +9,36 @@ using UnityEngine.UI;
 
 [BepInPlugin("sosarciel.grindingwheelreforgedequipment", "GrindingWheelReforgedEquipment", "1.0.0.0")]
 public class GrindingWheelReforgedEquipment : BaseUnityPlugin {
-	private void Start() {
-		var harmony = new Harmony("GrindingWheelReforgedEquipment");
-		harmony.PatchAll();
-	}
+    private void Start() {
+        Initialize();
+        var harmony = new Harmony("GrindingWheelReforgedEquipment");
+        harmony.PatchAll();
+    }
     public void OnStartCore() {
-		var dir = Path.GetDirectoryName(Info.Location);
-		var excel = dir + "/Recipe.xlsx";
-		var sources = Core.Instance.sources;
-		ModUtil.ImportExcel(excel, "recipes", sources.recipes);
-	}
+        var dir = Path.GetDirectoryName(Info.Location);
+        var excel = dir + "/Recipe.xlsx";
+        var sources = Core.Instance.sources;
+        ModUtil.ImportExcel(excel, "recipes", sources.recipes);
+    }
+    void Initialize(){
+        GWREUtil.CalibrationMaxValue = base.Config.Bind<bool>("General", "CalibrationMaxValue", false,
+            "一次校准就能达到最大值\nOne-time calibration reaches maximum value");
+    }
 }
 
 
 
 
 static class GWREUtil{
+    public static ConfigEntry<bool> CalibrationMaxValue;
     static public void TryChangeBase(Thing thing){
+
+        int rd(int val, bool toLow=false){
+            return GWREUtil.CalibrationMaxValue.Value
+                ? toLow ? 0 : val-1
+                : EClass.rnd(val);
+        }
+
         //thing.ApplyMaterial
         var sc = thing.sourceCard;
         var source = thing.source;
@@ -32,9 +46,11 @@ static class GWREUtil{
             var text = (Lang.langCode == Lang.LangCode.CN.ToString())
                 ? "无法校准。"
                 : "Calibration not possible.";
-            thing.Say(text, thing, null, null);
+            Msg.SetColor(Msg.colors.MutateBad);
+            Msg.Say(text);
             return;
         }
+
         bool success = false;
         int num = 120;
         bool flag3 = !thing.IsAmmo;
@@ -61,7 +77,7 @@ static class GWREUtil{
 
 
         if (source.offense.Length != 0){
-            var dim = source.offense[1] * thing.material.dice / (num + (flag3 ? EClass.rnd(25) : 0));
+            var dim = source.offense[1] * thing.material.dice / (num + (flag3 ? rd(25,true) : 0));
             if(dim>thing.c_diceDim){
                 thing.c_diceDim = dim;
                 success = true;
@@ -69,7 +85,7 @@ static class GWREUtil{
         }
 
         if (source.offense.Length > 2){
-            var hit = sockethit + source.offense[2] * thing.material.atk * 9 / (num - (flag3 ? EClass.rnd(30) : 0));
+            var hit = sockethit + source.offense[2] * thing.material.atk * 9 / (num - (flag3 ? rd(30) : 0));
             success |= SetBase(66, hit);
         }
 
@@ -78,30 +94,32 @@ static class GWREUtil{
             if(thing.IsWeapon || thing.IsAmmo)
                 ench = thing.encLV + ((thing.blessedState == BlessedState.Blessed) ? 1 : 0);
 
-            var dmg = socketdmg + ench + source.offense[3] * thing.material.dmg * 5 / (num - (flag3 ? EClass.rnd(30) : 0));
+            var dmg = socketdmg + ench + source.offense[3] * thing.material.dmg * 5 / (num - (flag3 ? rd(30) : 0));
             success |= SetBase(67, dmg);
         }
 
         if (source.defense.Length != 0)
-            success |= SetBase(64, source.defense[0] * thing.material.dv * 7 / (num - (flag3 ? EClass.rnd(30) : 0)));
+            success |= SetBase(64, source.defense[0] * thing.material.dv * 7 / (num - (flag3 ? rd(30) : 0)));
 
         if (source.defense.Length > 1){
             var ench = 0;
             if(!thing.IsWeapon && !thing.IsAmmo)
                 ench = (thing.encLV + ((thing.blessedState == BlessedState.Blessed) ? 1 : 0)) * 2;
 
-            var pv = socketpv + ench + source.defense[1] * thing.material.pv * 9 / (num - (flag3 ? EClass.rnd(30) : 0));
+            var pv = socketpv + ench + source.defense[1] * thing.material.pv * 9 / (num - (flag3 ? rd(30) : 0));
             success |= SetBase(65, pv);
         }
 
         if(success){
             var text = (Lang.langCode == Lang.LangCode.CN.ToString())
                 ? "成功校准。" : "Successfully success.";
-            thing.Say(text, thing, null, null);
+            Msg.SetColor(Msg.colors.MutateGood);
+            Msg.Say(text);
         }else{
             var failText = (Lang.langCode == Lang.LangCode.CN.ToString())
                 ? "校准失败。" : "Calibration failed.";
-            thing.Say(failText, thing, null, null);
+            Msg.SetColor(Msg.colors.MutateBad);
+            Msg.Say(failText);
         }
 
         bool SetBase(int ele, int a){
@@ -118,7 +136,7 @@ static class GWREUtil{
 [HarmonyPatch(nameof(TraitCrafter.Craft))]
 [HarmonyPatch(new [] { typeof(AI_UseCrafter) })]
 class TraitCrafter_Craft_Patch {
-	public static bool Prefix(TraitGrindstone __instance, AI_UseCrafter ai, ref Thing __result){
+    public static bool Prefix(TraitGrindstone __instance, AI_UseCrafter ai, ref Thing __result){
         SourceRecipe.Row source = __instance.GetSource(ai);
         if (source == null) return true;
         //TraitGrindstone
---
+++ Solution/SecureMultisize/SecureMultisize.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+[BepInPlugin("sosarciel.securemultisize", "SecureMultisize", "1.0.0.0")]
+[BepInProcess("Elin.exe")]
+public class EquipModify : BaseUnityPlugin {
+    public static new ManualLogSource Logger;
+
+    void Awake() {
+        Logger = base.Logger;
+        Initialize();
+        var harmony = new Harmony("SecureMultisize");
+        harmony.PatchAll();
+        Logger.LogInfo("Awake");
+    }
+    void Initialize(){
+        SMUtils.AllowDestroyCities = base.Config.Bind<bool>("General", "AllowDestroyCities", false,
+"允许大型生物破坏城市\nAllows large creatures to destroy cities");
+        SMUtils.RestrictAllLargeCreatures = base.Config.Bind<bool>("General", "RestrictAllLargeCreatures", false,
+"约束所有大型生物。如果未启用，则只约束友方大型生物。\nRestricts all large creatures. If disabled, only friendly large creatures are restricted.");
+    }
+}
+
+public static class SMUtils{
+    public static ConfigEntry<bool> AllowDestroyCities;
+    public static ConfigEntry<bool> RestrictAllLargeCreatures;
+
+}
+
+
+[HarmonyPatch(typeof(Chara))]
+[HarmonyPatch(nameof(Chara.CanDestroyPath))]
+public static class Chara_CanDestroyPath_Patch{
+    private static bool Prefix(Chara __instance, ref bool __result){
+        if (!__instance.IsMultisize) return true;
+        if (!__instance.IsPCFactionOrMinion && !SMUtils.RestrictAllLargeCreatures.Value) return true;
+
+        //始终无法破坏家园与帐篷
+        if(EClass._zone.IsPCFaction || EClass._zone is Zone_Tent){
+            __result = false;
+            return false;
+        }
+
+        if(EClass._zone is Zone_Civilized && !SMUtils.AllowDestroyCities.Value){
+            __result = false;
+            return false;
+        }
+
+        __result = true;
+        return false;
+    }
+}
