8322133226f6a17de0a2f53409ceed5f2f42e9c2
zwa73 <ssfen2419@126.com>
2025-01-01 02:58:04 +0800

    upg

--- Solution/CureMutationBodyPart/CureMutationBodyPart.cs
+++ Solution/CureMutationBodyPart/CureMutationBodyPart.cs
@@ -1,4 +1,4 @@
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +12,48 @@ public class CureMutationBodyPart : BaseUnityPlugin {
         harmony.PatchAll();
     }
 }
+public static class CMBPUtils {
+    public static void RemoveBodyPart(CharaBody cb,BodySlot bp){
+        var slots = cb.slots;
 
+        int num = slots.FindIndex((BodySlot a) => a == bp);
+        if (num != -1){
+            BodySlot bodySlot = slots[num];
+            if (bodySlot.thing != null)
+                cb.Unequip(bodySlot);
+
+            if (cb.slotMainHand == bodySlot)
+                cb.slotMainHand = null;
+
+            if (cb.slotOffHand == bodySlot)
+                cb.slotOffHand = null;
+
+            if (cb.slotRange == bodySlot)
+                cb.slotRange = null;
+
+            slots.RemoveAt(num);
+        }
+
+        ResetEquipIndex(cb);
+    }
+    public static void ResetEquipIndex(CharaBody cb){
+        cb.slots.ForEach(bp=>{
+            var index = cb.slots.FindIndex(s=>s==bp);
+            if(index!=-1 && bp.thing!=null)
+                bp.thing.c_equippedSlot = index+1;
+        });
+    }
+    public static void SubMutateBodyFeat(Chara c){
+        if(c.elements.GetElement(1644).Value>5){
+            //大于5特殊处理
+            c.elements.GetElement(60).vBase+=3;
+            c.elements.GetElement(79).vBase+=5;
+            c.elements.GetElement(77).vBase+=3;
+        }
+        c.elements.GetElement(1644).vBase--;
+        c.feat+=1;
+    }
+}
 
 [HarmonyPatch(typeof(Chara))]
 [HarmonyPatch(nameof(Chara.MutateRandom))]
@@ -29,16 +70,23 @@ class MutateRandomPatch {
                     select a;
                 bool notHasDeletableMutation = ie.All(e=>!__instance.elements.Has(e.id));
 
-                var bpids = __instance.body.slots
+                var bps = __instance.body.slots
                     .Where(bp=>bp.elementId!=45)//排除光源
                     .Where(bp=>bp.elementId!=44)//排除工具袋
-                    .Select(bp=>bp.elementId)
                     .ToList();
 
-                var vaildBpcount = bpids.ToArray().Length;
+                var vaildBpcount = bps.Count();
+
                 //尝试保留1手1身
-                if(bpids.Contains(35)) bpids.Remove(35);
-                if(bpids.Contains(32)) bpids.Remove(32);
+                var excludeIds = new List<int> { 35, 32 };
+                bps = bps.Where(bp=>{
+                    if(excludeIds.Contains(bp.elementId)){
+                        excludeIds.Remove(bp.elementId);
+                        return false;
+                    }
+                    return true;
+                }).ToList();;
+
                 //Msg.Say(string.Join(",", bpids));
                 //Msg.Say(" ");
                 //Msg.Say(notHasDeletableMutation+" "+vaildBpcount+" "+__instance.elements.Has(1644));
@@ -47,18 +95,15 @@ class MutateRandomPatch {
                 if (notHasDeletableMutation &&
                     vaildBpcount > 2 &&
                     __instance.elements.Has(1644)){
-                    var rid = bpids.RandomItem();
-                    //Msg.Say("remove "+ rid);
-                    __instance.body.RemoveBodyPart(rid);
+
+                    //优先尝试删除无装备肢体
+                    var noEquipBps = bps.Where(bp=>bp.thing==null).ToList();
+                    if(noEquipBps.Count()>0)
+                        CMBPUtils.RemoveBodyPart(__instance.body,noEquipBps.RandomItem());
+                    else __instance.body.RemoveBodyPart(bps.RandomItem().elementId);
+
                     //解除专长
-                    if(__instance.elements.GetElement(1644).Value>5){
-                        //大于5特殊处理
-                        __instance.elements.GetElement(60).vBase+=3;
-                        __instance.elements.GetElement(79).vBase+=5;
-                        __instance.elements.GetElement(77).vBase+=3;
-                    }
-                    __instance.elements.GetElement(1644).vBase--;
-                    __instance.feat+=1;
+                    CMBPUtils.SubMutateBodyFeat(__instance);
                     if (__instance.Chara.IsPC && WidgetEquip.Instance){
                         WidgetEquip.Instance.Rebuild();
                         EClass.ui.CloseLayers();
@@ -66,7 +111,9 @@ class MutateRandomPatch {
                         if (EClass.ui.IsInventoryOpen)
                             EClass.ui.ToggleInventory(false);
                     }
+
                     __instance.Chara.body.RefreshBodyParts();
+
                     if (EClass.core.IsGameStarted && __instance.pos != null){
                         __instance.PlaySound(ether ? "mutation_ether" : "mutation");
                         __instance.PlayEffect("mutation");
@@ -86,7 +133,14 @@ class MutateRandomPatch {
     }
 }
 
-
+[HarmonyPatch(typeof(CharaBody))]
+[HarmonyPatch(nameof(CharaBody.RemoveBodyPart))]
+[HarmonyPatch(new[] { typeof(int) })]
+class CharaBody_RemoveBodyPart_Patch {
+    static void Postfix(CharaBody __instance,int ele){
+        CMBPUtils.ResetEquipIndex(__instance);
+    }
+}
 
 [HarmonyPatch(typeof(Feat))]
 [HarmonyPatch(nameof(Feat.Apply))]
